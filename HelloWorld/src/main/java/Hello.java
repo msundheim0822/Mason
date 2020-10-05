3646854f3143ac9dbf5f4201c7ccbdd75df480a7
Mason Sundheim <msundheim0822@gmail.com>
2020-10-05 17:34:07 -0400

    First Commit

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Mason Sundheim
+ * msundheim0822@gmail.com
+ * 10/5/20
+ * purpose: My first Hello World program in IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Mason!");
+    }
+}
